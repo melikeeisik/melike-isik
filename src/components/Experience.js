ffdd5b7ac8ec33ea9caeeba8779d2b47675e9913
Melike ISIK <melike_gs_8-@hotmail.com>
2024-01-22 00:14:14 +0300

    Experience componenti güncellendi

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -72,7 +72,7 @@ function Experience() {
                 <div style={{position:"relative"}}>
                 <FontAwesomeIcon style={{color:"#2196f3", position:"absolute", top:"20px",fontSize:"20px"}} icon="fa-circle" />
                     <div style={{position:"absolute", backgroundColor:"#fff",width:"15px", height:"15px", borderRadius:"999px",top:"22.5px", left: minPage ? "2.5px" :"3px"}}></div>
-                    <div style={{backgroundColor:"#2196f3",width:"0.5px" , height: minPage ? "180px":"120px", position:"absolute",left:"10px",top:"40px"}}/>
+                    <div style={{backgroundColor:"#2196f3",width:"0.5px" , height: minPage ? "200px":"120px", position:"absolute",left:"10px",top:"40px"}}/>
                 </div>
             </div>
             <div className={style.positionBox}>
@@ -93,7 +93,7 @@ function Experience() {
             <div className={style.experinceSeperate}>
                 <div style={{position:"relative"}}>
                     <FontAwesomeIcon style={{color:"#2196f3", position:"absolute", top:"18px",fontSize:"20px"}} icon="fa-circle" />
-                    <div style={{position:"absolute", backgroundColor:"#fff",width:"15px", height:"15px", borderRadius:"999px",top:"20.5px", left:"3px"}}></div>
+                    <div style={{position:"absolute", backgroundColor:"#fff",width:"15px", height:"15px", borderRadius:"999px",top:"20.5px", left: minPage ? "2.5px" :"3px"}}></div>
                     <div style={{backgroundColor:"#2196f3",width:"0.5px" , position:"absolute",left:"10px",top:"40px"}}/>
                 </div>
             </div>
