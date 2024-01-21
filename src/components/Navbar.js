222602e20a5aaa0bd5e7293f0069a0e5588e77a7
Melike ISIK <melike_gs_8-@hotmail.com>
2024-01-21 21:18:03 +0300

    Responsive özellik için toggle menü eklendi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,24 @@ import React, { useEffect, useState , useRef} from 'react'
 import style from "../style.module.css"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar , faBars} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faStar)
+library.add(faStar,faBars)
 
 function Navbar({scrollToSection, sticky, page}) {
+  const [screen, setScreen] = useState("")
   const list = document.getElementsByTagName("li")
+  const [openMenu, setOpenMenu] = useState(false) 
+  useEffect(() =>{
+    const handleResize = () =>{
+      setScreen(window.innerWidth)
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [])
+
   useEffect(() =>{
     [...list].forEach(element => {
       if(element.innerHTML==page){
@@ -19,20 +31,27 @@ function Navbar({scrollToSection, sticky, page}) {
       
   },[page])
 
+  const handleMenu = () =>{
+    setOpenMenu(true)
+  }
 
   return (
     <div>
       <div className={sticky ? style.headerSticky : style.header}>
             <div className={style.headerIcon}>
-                <FontAwesomeIcon icon="fa-solid fa-star" />
+                <FontAwesomeIcon icon="fa-solid fa-star" /> 
             </div>
             <div className={style.headerList}>
-                <ul>
+                <input type="checkbox" id='menu-toggle' className={style.menuToggle}/>
+                <ul className={style.menu} style={{display:screen <800 && !openMenu  ? "none" :"flex" }} > 
                     <li onClick={() => scrollToSection("anasayfa")} >Anasayfa</li>
                     <li onClick={() => scrollToSection("hakkimda")} >Hakkımda</li>
                     <li onClick={() => scrollToSection("deneyim")} >Deneyimlerim</li>
                     <li onClick={() => scrollToSection("iletisim")} >İletişim</li>
                 </ul>
+                <label htmlFor='menu-toggle' onClick={handleMenu} className={style.mobileMenu}>
+                  <FontAwesomeIcon icon="fa-solid fa-bars" /> 
+                </label>
             </div>
         </div>
     </div>
